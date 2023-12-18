008f7f0faa9563fe8dfa826435a5f02e750e6b8b
Festus Kogo <fkogoson@gmail.com>
2023-12-18 16:22:02 +0300

    Refactor code

--- artistsapp/admin.py
+++ artistsapp/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Song, Album, Artist
 
 # Register your models here.
-admin.site.register(Song)
-admin.site.register(Album)
-admin.site.register(Artist)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("name", "stage_name", "country", "dob",)
+    # prepopulated_fields = {"slug": ("stage_name")}
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("name", "album", "artist", "genre",)
+    # prepopulated_fields = {"slug": ("stage_name")}
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("artist", "name", "release_date",)
+    # prepopulated_fields = {"slug": ("stage_name")}
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
